f549f9985498a89bba929d9da26f27bfc92f2e43
Himanshu <himanshuojha56@gmail.com>
2020-09-28 17:46:55 +0530

    model update

--- microFlipkart/products/admin.py
+++ microFlipkart/products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, ProductVariant
+
+#admin.site.register(Product)
+#admin.site.register(ProductVariant)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(ProductVariant) 
+class ProductVarientAdmin(admin.ModelAdmin):
+    list_display = ('product', 'color', 'size', 'texture', 'price')
